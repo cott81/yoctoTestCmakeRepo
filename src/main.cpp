b1f740a2205834d1a23a6594577df7bc509d7b85
Ich <ich@ich.com>
2017-10-06 17:15:33 +0200

    yocto cmake test package

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+ 
+using namespace std;
+
+//int func(int);
+ 
+int main(int argc, char *argv[]) {
+	//int a = func(0);
+	int a = 10;
+    cout << "Hello world ! " << a << endl;
+    return 0;
+}
